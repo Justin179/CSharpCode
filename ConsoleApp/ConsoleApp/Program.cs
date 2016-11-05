39c5fefad6930a2ad0a2d5f06bc2a73ad3eebab8
Justin179 <justin.uob@gmail.com>
2016-11-05 17:13:37 +0800

    Generic Impl

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp.Generics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,9 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hi");
+
+
+
         }
     }
 }
---
+++ ConsoleApp/UnitTestProject1/GenericTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp.Generics;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class GenericTest
+    {
+
+        [TestMethod]
+        public void NullableTest()
+        {
+            var nullable = new Nullable<int>(5);
+            Assert.AreEqual(true,nullable.HasValue);
+            Assert.AreEqual(5,nullable.GetValueOrDefault());
+
+            var nullable2 = new Nullable<int>();
+            Assert.AreEqual(false, nullable2.HasValue);
+            Assert.AreEqual(0, nullable2.GetValueOrDefault());
+        }
+        
+        
+        [TestMethod]
+        public void GenericDictionaryTest()
+        {
+            var genericDic = new GenericDictionary<string, Book>();
+            var book = new Book();
+            genericDic.Add("123", book);
+
+        }
+        
+        [TestMethod]
+        public void GenericListTest()
+        {
+            // Arrange
+            var books = new GenericList<Book>();
+            var book = new Book() { Name = "網站開發美學", Price = 380 };
+            books.Add(book);
+            // Act
+            var numbers = new GenericList<int>();
+            numbers.Add(1);
+            numbers.Add(2);
+            // Assert
+        }
+    }
+}
