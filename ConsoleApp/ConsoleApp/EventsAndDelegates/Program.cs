94ff4e22052bdd9618ef0710e414f2d058b260be
Justin179 <justin.uob@gmail.com>
2016-11-11 11:34:20 +0800

    Extension Method

--- ConsoleApp/ConsoleApp/EventsAndDelegates/Program.cs
+++ ConsoleApp/ConsoleApp/EventsAndDelegates/Program.cs
@@ -8,20 +8,20 @@ namespace ConsoleApp.EventsAndDelegates
     class Program
     {
 
-        public static void Main(string[] args)
-        {
-            var video = new Video() {Title = "video one"};
-            var videoEncoder = new VideoEncoder(); // publisher
+        //public static void Main(string[] args)
+        //{
+        //    var video = new Video() {Title = "video one"};
+        //    var videoEncoder = new VideoEncoder(); // publisher
 
             
-            var mailService = new MailService(); // subscriber
-            var textMessage = new TextMessage(); // subscriber
-            // 註冊: 把subscriber的方法指向publisher的主物件(注意是+=)
-            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
-            videoEncoder.VideoEncoded += textMessage.OnVideoEncoded;
+        //    var mailService = new MailService(); // subscriber
+        //    var textMessage = new TextMessage(); // subscriber
+        //    // 註冊: 把subscriber的方法指向publisher的主物件(注意是+=)
+        //    videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+        //    videoEncoder.VideoEncoded += textMessage.OnVideoEncoded;
 
 
-            videoEncoder.Encode(video);
-        }
+        //    videoEncoder.Encode(video);
+        //}
     }
 }
---
+++ ConsoleApp/ConsoleApp/ExtensionMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.ExtensionMethods
+{
+    class Program
+    {
+        //public static void Main(string[] args)
+        //{
+        //    string post = "This is a very long string characters blah blah blah blah...";
+        //    var shortenedString = post.Shorten(5);
+        //    Console.WriteLine(shortenedString);
+
+        //    // 這裡用IEnumerable接是為了Extension Methods, 因為list有實作，因此可用IEnumerable接
+        //    IEnumerable<int> list = new List<int>(){1,2,3,4,5};
+        //    int max = list.Max();
+        //    Console.WriteLine(max);
+        //}
+
+    }
+}
