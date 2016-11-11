674cf1695802f054112a85d62eb0b6944f2cb7c1
Justin179 <justin.uob@gmail.com>
2016-11-11 12:51:52 +0800

    Linq

---
+++ ConsoleApp/ConsoleApp/Linq/BookRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Linq
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+
+            return new List<Book>()
+            {
+                new Book() {Title = "A", Price = 10.0F},
+                new Book() {Title = "B", Price = 11.0F},
+                new Book() {Title = "C", Price = 12.0F},
+                new Book() {Title = "D", Price = 13.0F},
+                new Book() {Title = "E", Price = 14.0F}
+            };
+
+
+        }
+    }
+}
